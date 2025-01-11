e8e59c70e9a918097b0143bf3af011b2b855ec09
Vikas J <emailvikas81@gmail.com>
2025-01-11 15:40:58 +1100

    Initial commit

---
+++ CodedUITestProject7/CodedUITest2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using CUITe.Controls.HtmlControls;
+using CodedUITestProject7.ObjectRepository;
+
+namespace CodedUITestProject7
+{
+    /// <summary>
+    /// Summary description for CodedUITest2
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest2
+    {
+        public CodedUITest2()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            CUITe_BrowserWindow.Launch("http://uat16.compliancewire.com/");
+            LoginPage uLogin = CUITe_BrowserWindow.GetBrowserWindow<LoginPage>();
+            Assert.IsTrue(uLogin.spnProcessingPleaseWait.Exists, "Login Page not found");
+            uLogin.txtLOGINCTLT1.SetText("NSaxena");
+            uLogin.txtLOGINCTLT2.SetText("a");
+            uLogin.txtLOGINCTLT3.SetText("edu");
+            uLogin.spnProcessingPleaseWait.Click();
+                
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        //Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {        
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            CUITe_BrowserWindow.CloseAllBrowsers();
+            
+        }
+
+        //Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {        
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+        }
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+    }
+}
---
+++ CodedUITestProject7/ObjectRepository/LoginPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using CUITe.Controls.HtmlControls;
+
+namespace CodedUITestProject7.ObjectRepository
+{
+    public class LoginPage //: CUITe_BrowserWindow
+    {
+        public CUITe_HtmlEdit txtLOGINCTLT1 = new CUITe_HtmlEdit("Id=LOGINCTL_T1");
+        public CUITe_HtmlPassword txtLOGINCTLT2 = new CUITe_HtmlPassword("Id=LOGINCTL_T2");
+        public CUITe_HtmlEdit txtLOGINCTLT3 = new CUITe_HtmlEdit("Id=LOGINCTL_T3");
+        public CUITe_HtmlSpan spnProcessingPleaseWait = new CUITe_HtmlSpan("Id=IAccept");
+        //public new string sWindowTitle = "uat16. Eduneering";
+        public CUITe_HtmlHyperlink lnkTestTrainingItem = new CUITe_HtmlHyperlink("InnerText=Test Training item;Title=Click to Launch Course");
+        public CUITe_HtmlHyperlink lnkContinue = new CUITe_HtmlHyperlink("Id=continueLink");
+        public CUITe_HtmlDiv divTemp1 = new CUITe_HtmlDiv("Id=divCDWindow");
+    }
+}
